8e961da3d71a80dd42b5137718349041b451c1f1
John Lazzari <johnlazzari@pop-os.localdomain>
2022-04-05 14:49:59 -0400

    added batches, training accuracy, and stacked lstm

--- LSTM.py
+++ LSTM.py
@@ -14,6 +14,7 @@ class LSTM(nn.Module):
         self.num_layers = num_layers
         self.input_size = input_size
         self.hidden_size = hidden_size
+
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, 
                             num_layers=num_layers, batch_first=True) #lstm
         self.fc_1 = nn.Linear(hidden_size, 128)
@@ -40,35 +41,70 @@ print(np.shape(train_set))
 targets = []
 training_inputs = []
 end_token = torch.zeros([1, 20])
-# quick test for targets
-for i in range(len(train_set)):
-    one_hot_target = torch.Tensor(np.array(train_set[i][-2]))
-    targets.append((one_hot_target == 1).nonzero(as_tuple=True)[0])
-    new_train = torch.Tensor(np.array(train_set[i][:-2]))
-    new_train = torch.cat((new_train, end_token), dim=0)
-    new_train = torch.unsqueeze(new_train, dim=0)
-    training_inputs.append(new_train)
 
-model = LSTM(20, 40, 1).to(device)
+batch_size = 256
+batch_targets = []
+batch_inputs = []
+# quick way to implement batches, should do this better
+for i in range(len(train_set)):
+
+    # create the batches and convert to tensors
+    if i % batch_size == 0 and i != 0:
+        targets.append(torch.cat(batch_targets, dim=0))
+        training_inputs.append(torch.stack(batch_inputs, dim=0))
+        # reset
+        batch_targets = []
+        batch_inputs = []
+
+    # Get the one hot of the second to last character (before end character)
+    one_hot_target = torch.Tensor(np.array(train_set[i][-2]))
+    # Get it in the form of an index for cross entropy
+    batch_targets.append((one_hot_target == 1).nonzero(as_tuple=True)[0])
+
+    # Get the last 51 sequence elements
+    cur_sequence = torch.Tensor(np.array(train_set[i][-51:]))
+    # remove the last 2 elements
+    cur_sequence = torch.Tensor(cur_sequence[:-2])
+    # concatenate the remaining sequence with the end token
+    cur_sequence = torch.cat((cur_sequence, end_token), dim=0)
+
+    # If the sequence is not 50 elements, pad it with zeros in the beginning
+    while cur_sequence.shape[0] < 50:
+        cur_sequence = torch.cat((end_token, cur_sequence), dim=0)
+
+    # append to batch list
+    batch_inputs.append(cur_sequence)
+
+# Standard training components
+model = LSTM(20, 60, 4).to(device)
 lr = .001
-epochs = 5
+epochs = 100
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
 for epoch in range(epochs):
+    # keep track of epoch loss
     running_loss = 0
+    correct = 0
     for i, sequence in enumerate(training_inputs):
         # Put data to devices
         sequence, targets[i] = sequence.to(device), targets[i].to(device)
 
-        # Standard training protocol, takes very long too much data
-        # Might need to implement batches, maybe add layers or make bidirectional
         optimizer.zero_grad()
         prediction = model(sequence)
+        # Get the final hidden state for the batch, should be the last 256
+        prediction = prediction[-256:]
         loss = criterion(prediction, targets[i])
+
+        # Getting predictions and training accuracy
+        pred = prediction.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(targets[i].view_as(pred)).sum().item()
+
+        # keep track of loss
         running_loss += loss.item()
+        # Update parameters
         loss.backward()
         optimizer.step()
 
     running_loss /= len(training_inputs)
-    print("Epoch {} training loss: {}".format(epoch, running_loss))
+    print("Epoch {} training loss: {}, Training acc: {}".format(epoch, running_loss, 100 * correct / (len(training_inputs)*256)))
